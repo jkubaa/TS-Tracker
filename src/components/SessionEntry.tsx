cf24a73bb6c6f9f65fca8731b5857bad1c4ba8c0
Kuba <29735915+jkubaa@users.noreply.github.com>
2024-02-10 17:16:14 +0000

    - Sessions are now fetched from TeamSport API - Page is populated with sessions (currently only Adult Races and member 342) - Filters for dates and session types added

--- src/components/SessionEntry.tsx
+++ src/components/SessionEntry.tsx
@@ -1,11 +1,19 @@
-function SessionEntry() {
+
+
+function SessionEntry(props : any) {
+    
+    const [date, time] = props.sessionDate.split("T")
+
+    const peopleBooked = props.sessionCapacity - props.sessionFreeSpots
+
     return (
-        <section className="text-white border border-white rounded-lg">
-            <h2>Session name</h2>
-            <p>Date: <span className="font-semibold">01/01/2024</span></p>
-            <p>Slots: <span className="font-semibold">0 / 0</span></p>
+        <section className="text-white border border-white rounded-lg text-center m-2 my-4">
+            <h2 className="text-lg font-semibold">{props.sessionName}</h2>
+            <p>Time: <span className="font-semibold">{time}</span></p>
+            <p>Slots left: <span className="font-semibold">{props.sessionFreeSpots} / {props.sessionCapacity}</span></p>
+            <p className="text-sm"><span className="font-semibold">{peopleBooked}</span> racers booked</p>
             <a className="font-semibold text-stroke-3 rounded-lg bg-gradient-to-r from-red-300 to-red-500 hover:from-red-200 hover:to-red-400 
-                            shadow-none hover:shadow-lg hover:shadow-gray-600 px-2.5 py-2 text-xl select-none cursor-pointer block w-[5%] my-2">Book</a>
+                            shadow-none hover:shadow-lg hover:shadow-gray-600 px-2.5 py-2 text-xl select-none cursor-pointer block mx-5 my-3">Book</a>
         </section>
     )
 }
--- src/components/SessionsList.tsx
+++ src/components/SessionsList.tsx
@@ -1,7 +1,12 @@
+import useSWR from "swr"
+import axios from "axios";
+
 import { useEffect, useState } from "react"
 
+// Components
 import SessionEntry from "./SessionEntry";
-import useSWR, { Fetcher } from 'swr'
+import SessionFilter from "./SessionFilter";
+//import SessionFilter from "./SessionFilter";
 
 interface AccessTokenResponse {
     ClientKey: string,
@@ -9,24 +14,62 @@ interface AccessTokenResponse {
     AccessToken: string
 }
 
-function SessionsList() {
-    const accessTokenUrl: string = "https://backend.sms-timing.com/api/connectioninfo/encrypted?message=U2FYdGVkX19I1GUtROBK1w%2B2YxHDxGwW582h0a%2BLf2KQihI1sndOHoPnZGr1AnVE&locationType=3&type=booking"
-    const fetcher = (url: string) => fetch(url).then(res => res.json())
-    const { data, error, isLoading } = useSWR<AccessTokenResponse>(accessTokenUrl, fetcher, { revalidateOnFocus: false})
-    const apiKey = data?.AccessToken
-    console.log(apiKey)
+interface SessionResponse {
+    proposals: any[]
+}
 
-    //const [sessions, setSessions] = useState([])
+export default function SessionsList() {
+    const [startDate, setStartDate] = useState(new Date())
+    const [productId, setProductId] = useState("666353")
+
+    // Fetch sessions from the API
+    const apiKey = FetchAccessToken()
+    const sessionsUrl: string = "https://booking-api18.sms-timing.com/api/dayplanner/dayplannerauto/teamsportnewcastle?date=" + startDate.toISOString()
+
+    const tsApiBody = {
+        "dynamicLines": null,
+        "pageid": "660754",
+        "productId": productId,
+        "quantity": 1
+    }
+    
+    const tsApiHeaders = {
+        method: "POST",
+        "X-Fast-AccessToken": "39cweswbciibriiberr"
+    }
+
+    const fetcher = (url: string) => 
+        axios.post(url, tsApiBody, { headers: tsApiHeaders })
+        .then(response => response.data)
+
+    const { data, error, isLoading } = useSWR<SessionResponse>(sessionsUrl, fetcher)
+
+    const sessionEntries = data?.proposals.map((proposal, index) => (
+        <SessionEntry
+            sessionName={proposal.blocks[0].block.name}
+            sessionDate={proposal.blocks[0].block.start}
+            sessionCapacity={proposal.blocks[0].block.capacity}
+            sessionFreeSpots={proposal.blocks[0].block.freeSpots}
+            key={index}
+        />
+    ));
 
     return (
         <>
-            <h1 className="text-white text-center text-2xl font-semibold">Sessions List</h1>
-            <SessionEntry />
+        <h1 className="text-white text-center text-2xl font-semibold">Sessions List</h1>
+        <SessionFilter startDate={startDate} setStartDate={setStartDate} setProductId={setProductId}/>
+        <div className="grid grid-cols-4">
+            {sessionEntries}
+        </div>
         </>
     );
 }
 
-
-
-
-export default SessionsList;
+function FetchAccessToken() {
+    const accessTokenUrl: string = "https://backend.sms-timing.com/api/connectioninfo/encrypted?message=U2FYdGVkX19I1GUtROBK1w%2B2YxHDxGwW582h0a%2BLf2KQihI1sndOHoPnZGr1AnVE&locationType=3&type=booking"
+    const fetcher = (url: string) => 
+        fetch(url)
+        .then(response => response.json())
+    const { data, error } = useSWR<AccessTokenResponse>(accessTokenUrl, fetcher, { revalidateOnFocus: false} )
+    return data?.AccessToken
+}
