743858e9ee8f809e35af5283d4f142b0f04658b7
Kuba <29735915+jkubaa@users.noreply.github.com>
2024-02-11 02:23:06 +0000

    Fixes and styling changes - AccessToken is now fetched from TS API - Added Material Tailwind Library - Styled filters and session entries - New datepicker

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
- 
-export default {
+const withMT = require("@material-tailwind/react/utils/withMT")
+
+export default withMT({ 
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+})
